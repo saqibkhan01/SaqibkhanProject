508afd9bb0ef1ac60bbc7794037d799892bb6892
saqibkhan01 <muhammadsaqibkhan01@gmail.com>
2020-09-13 13:21:07 -0700

    first commit

---
+++ Student/Controllers/StudentModelsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Student.Models;
+
+namespace Student.Controllers
+{
+    public class StudentModelsController : Controller
+    {
+        private StudentDBContext db = new StudentDBContext();
+
+        // GET: StudentModels
+        public ActionResult Index(string searchString)
+        {
+            var stds = from m in db.Students
+                       select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                stds = stds.Where(s => s.Name.Contains(searchString));
+            }
+
+            return View(stds);
+        }
+
+
+        // GET: StudentModels/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentModel studentModel = db.Students.Find(id);
+            if (studentModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentModel);
+        }
+
+        // GET: StudentModels/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentModels/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,DateofBirth,Gender,Fee")] StudentModel studentModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(studentModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(studentModel);
+        }
+
+        // GET: StudentModels/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentModel studentModel = db.Students.Find(id);
+            if (studentModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentModel);
+        }
+
+        // POST: StudentModels/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,DateofBirth,Gender,Fee")] StudentModel studentModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(studentModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(studentModel);
+        }
+
+        // GET: StudentModels/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentModel studentModel = db.Students.Find(id);
+            if (studentModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentModel);
+        }
+
+        // POST: StudentModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            StudentModel studentModel = db.Students.Find(id);
+            db.Students.Remove(studentModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Student/Models/StudentModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Student.Models
+{
+    public class StudentModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime DateofBirth { get; set; }
+        public string Gender { get; set; }
+        public decimal Fee { get; set; }
+    }
+
+    public class StudentDBContext : DbContext
+    {
+        public DbSet<StudentModel> Students { get; set; }
+    }
+}
